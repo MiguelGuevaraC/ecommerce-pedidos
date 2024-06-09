bac0b4ae5c709f7c3c69cb7410bbba53ebc6f994
JoshiiMaru <joshua2036david@gmail.com>
2024-06-09 14:31:13 -0500

    ecommerce

--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,21 +1,74 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { Product } from '../../../interfaces/product';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     RouterOutlet,
+    CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
   ],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response => {
+      this.products = response.data;
+    });
+  }
+
+  incrementStock(product: Product) {
+    if (product.stock) {
+      product.stock++;
+    } else {
+      product.stock = 1;
+    }
+  }
+
+  decrementStock(product: Product) {
+    if (product.stock && product.stock > 1) {
+      product.stock--;
+    }
+  }
+
+  addToCart(product: Product) {
+    console.log(`Added ${product.stock} of ${product.name} to the cart`);
+  }
+
+  getProductImage(route: string){
+    return this.productService.getProductImage(route);
+  }
 }
